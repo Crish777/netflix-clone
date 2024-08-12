117a438673db966f8cf84b1bb288dfcd8d7df749
Cristhian Herrera <cristhianeleden@gmail.com>
2024-08-12 10:47:54 -0500

    [Fix] navbar

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -19,13 +19,18 @@ const Watch = () => {
     }
   }, [hideNav])
   return (
-    <div className='h-screen w-screen bg-black' onMouseMove={() => setHideNav(false)}>
-      <nav className={`fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70 ${hideNav ? 'opacity-0' : 'opacity-100'} transition duration-300`}>
-        <AiOutlineArrowLeft onClick={() => router.push('/')} className='text-white cursor-pointer' size={40} />
-        <p className='text-white text-1xl lg:text-3xl font-bold'><span className='font-light'>Watching: </span>{data?.title}</p>
-      </nav>
-      <video autoPlay controls src={data?.videoUrl} className='w-full h-full'></video>
-    </div>
+    <>
+      <p className="text-red-700 font-bold">
+        Este sitio es un proyecto demostrativo y no está afiliado con Netflix. No se recopila ninguna información real.
+      </p>
+      <div className='h-screen w-screen bg-black' onMouseMove={() => setHideNav(false)}>
+        <nav className={`fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70 ${hideNav ? 'opacity-0' : 'opacity-100'} transition duration-300`}>
+          <AiOutlineArrowLeft onClick={() => router.push('/')} className='text-white cursor-pointer' size={40} />
+          <p className='text-white text-1xl lg:text-3xl font-bold'><span className='font-light'>Watching: </span>{data?.title}</p>
+        </nav>
+        <video autoPlay controls src={data?.videoUrl} className='w-full h-full'></video>
+      </div>
+    </>
   )
 }
 
